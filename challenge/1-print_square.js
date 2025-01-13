3839efe08e3f1399c937b0f431bfe2b7348895c4
Basco <martinez.xabi@laposte.net>
2025-01-13 11:45:17 +0100

    .

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -14,11 +14,7 @@ if (process.argv.length <= 2) {
   process.exit(1)
 }
 
-const size = parseInt(process.argv[2])
-
-if (isNaN(size) || size <= 0) {
-  process.stderr.write("Need positive number\n");
-}
+const size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
   for (let j = 0 ; j < size ; j ++) {
